eedb4af0672578a31dcc31e5e6293d29e882c7d0
Lamp <giaphuc29082004@gmail.com>
2025-10-20 17:20:31 +0700

    Fix socket logic, Fix FE

--- frontend/src/utils/chat/mergeBlock.js
+++ frontend/src/utils/chat/mergeBlock.js
@@ -1,15 +1,15 @@
 export default function mergeBlock(lastBlock, newBlock) {
     if (!lastBlock || !newBlock) return false;
-
-    if (lastBlock.blockType === 'text' && newBlock.blockType === 'text') {
-        lastBlock.textContent += newBlock.textContent;
+    
+    if (lastBlock.block_type === 'text' && newBlock.block_type === 'text') {
+        lastBlock.text_content += newBlock.text_content;
         return true;
     }
-    if (lastBlock.blockType === 'image' && newBlock.blockType === 'image') {
+    if (lastBlock.block_type === 'image' && newBlock.block_type === 'image') {
         lastBlock.image_urls = lastBlock.image_urls.concat(newBlock.image_urls);
         return true;
     }
-    if (lastBlock.blockType === 'video' && newBlock.blockType === 'video') {
+    if (lastBlock.block_type === 'video' && newBlock.block_type === 'video') {
         lastBlock.video_urls = lastBlock.video_urls.concat(newBlock.video_urls);
         return true;
     }
